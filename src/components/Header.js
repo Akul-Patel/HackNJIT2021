cc4c257f042c353ed97f30fcbb60b1b06bccafdd
Akul <akul24700@gmail.com>
2021-11-13 18:20:26 -0500

    New changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,9 @@ import LanguageIcon from '@material-ui/icons/Language';
 // import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { Avatar, Button } from '@material-ui/core';
 import { Link } from 'react-router-dom'
-<<<<<<< HEAD
 import { useNavigate } from 'react-router-dom';
-=======
 import AuthNav from './auth-nav';
 
->>>>>>> f6d7f0512b3625a5c2771228f5813bb893127af3
 
 function Header() {
     const history = useNavigate();
