dd4d3175d69b1f6e5760e6108dd19b9db9f324bf
ramvyas07 <ramvyas089@gmail.com>
2021-11-14 03:28:50 -0500

    final

--- src/components/profile-button.js
+++ src/components/profile-button.js
@@ -2,12 +2,13 @@ import { Button } from '@material-ui/core';
 import { useNavigate } from 'react-router-dom';
 
 
-function ProfileButton({name}) {
+function ProfileButton({name,picture}) {
     const history = useNavigate();
 
     return ( 
         
            <Button onClick={() => history('/profile_button')}>Hey, {name}</Button>
+          
             
     
     
